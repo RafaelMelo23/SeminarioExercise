d9c511f8df4f06844d0075776f1d28b14ceca634
RafaelMelo23 <eooraff@gmail.com>
2024-07-26 15:38:12 -0300

    seminariosystem

---
+++ domain/test/Local.java
@@ -0,0 +1,20 @@
+package Seminariosys.domain.test;
+
+public class Local {
+    private String endereco;
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String toString() {
+        return "Local: "+endereco;
+    }
+}
